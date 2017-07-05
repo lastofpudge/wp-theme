63af6fa20136f19b9e5bc6dc388ec392dd259df1
lastofpudge <iluxor@yandex.ru>
2017-07-05 22:41:35 +0300

    pretty

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -14,9 +14,25 @@ class Controller
         $data = Timber::get_context();
         $data['test'] = '1';
 
+        /**
+         * polylang theme strings wrapper
+         */
         // $data['pll_e'] = TimberHelper::function_wrapper('pll_e');
+
+        /**
+         * theme option field
+         */
         // $data['vk_url'] = carbon_get_theme_option('crb_vkontakte_url');
+
+        /**
+         * test posts
+         */
         // $data['test_posts'] = Timber::get_posts('post_type=test&numberposts=-1');
+
+        /**
+         * main menu, use in twig: {{ menu }}
+         */
+        // $data['menu'] = TimberHelper::function_wrapper('wp_nav_menu', array('theme_location' => 'header_menu', 'container'=> false, 'menu_class'=> false));
 
         add_action('breads_func', self::render_pagination());
 
