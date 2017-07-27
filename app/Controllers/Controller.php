9f68dd400ac6ea28206cf9a307de172f79020996
Andrey <iluxor@yandex.ru>
2017-07-27 02:27:09 +0000

    Apply fixes from StyleCI

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -14,22 +14,22 @@ class Controller
         $data = Timber::get_context();
         $data['test'] = '1';
 
-        /**
+        /*
          * polylang theme strings wrapper
          */
         // $data['pll_e'] = TimberHelper::function_wrapper('pll_e');
 
-        /**
+        /*
          * theme option field
          */
         // $data['vk_url'] = carbon_get_theme_option('crb_vkontakte_url');
 
-        /**
+        /*
          * test posts
          */
         // $data['test_posts'] = Timber::get_posts('post_type=test&numberposts=-1');
 
-        /**
+        /*
          * main menu, use in twig: {{ menu }}
          */
         // $data['menu'] = TimberHelper::function_wrapper('wp_nav_menu', array('theme_location' => 'header_menu', 'container'=> false, 'menu_class'=> false));
@@ -45,11 +45,10 @@ class Controller
      */
     public static function render_pagination()
     {
-        return function ()
-        {
-            if (function_exists('yoast_breadcrumb'))
-            {
+        return function () {
+            if (function_exists('yoast_breadcrumb')) {
                 yoast_breadcrumb('', '');
+
                 return;
             }
             echo '<pre style="background-color: orange; padding:3px; color: #fff;">Plugin Youst Seo is not active!</pre>';
