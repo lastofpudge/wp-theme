5a19fcfef6c63e3925dde4f9408b86465968e1c0
Andrey <iluxor@yandex.ru>
2017-11-07 20:20:59 +0000

    Apply fixes from StyleCI

--- page-about.php
+++ page-about.php
@@ -3,6 +3,6 @@
  * Template Name: Карта сайта
  */
 
-require_once(__DIR__ . '/app/Controllers/pageController.php');
+require_once __DIR__.'/app/Controllers/pageController.php';
 
-Timber::render(array('/pages/about.twig'), $d->index());
+Timber::render(['/pages/about.twig'], $d->index());
