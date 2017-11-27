960b62e2dd85b1944404aed393b32a45fcc2deef
Andrey <iluxor@yandex.ru>
2017-11-28 01:33:09 +0200

    Update webpack.mix.js
    fix laravel-mix assets compile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
 
-mix.js('assets/src/scripts/bundle.js', 'assets/dist/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.setPublicPath('assets');
+
+mix.js('assets/src/scripts/bundle.js', 'assets/dist/js/bundle.js')
+    .sass(
+        'assets/src/styles/bundle.scss',
+        'assets/dist/css/').options({
+          processCssUrls: false
+    });
