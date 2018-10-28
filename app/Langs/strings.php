f36e1b43dcac91950f8b7a2c265dc26044bdffd2
Andrey <iluxor@yandex.ru>
2018-10-28 21:36:50 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Langs/strings.php
+++ app/Langs/strings.php
@@ -1,19 +1,23 @@
 <?php
 
-function crb_get_i18n_suffix() {
+function crb_get_i18n_suffix()
+{
     $suffix = '';
-    if ( ! defined( 'ICL_LANGUAGE_CODE' ) ) {
+    if (!defined('ICL_LANGUAGE_CODE')) {
         return $suffix;
     }
-    $suffix = '_' . ICL_LANGUAGE_CODE;
+    $suffix = '_'.ICL_LANGUAGE_CODE;
+
     return $suffix;
 }
 
 // translate theme options
-function crb_get_i18n_theme_option( $option_name ) {
+function crb_get_i18n_theme_option($option_name)
+{
     $suffix = crb_get_i18n_suffix();
-    return carbon_get_theme_option( $option_name . $suffix );
+
+    return carbon_get_theme_option($option_name.$suffix);
 }
 
 // test text
-pll_register_string('test_string', 'test_string', 'THEME','');
+pll_register_string('test_string', 'test_string', 'THEME', '');
