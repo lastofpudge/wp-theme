0e5d2adf38b4d4ea729518646ddd6dff82680c99
Andrey D <iluxor1991@gmail.com>
2018-10-28 22:25:06 +0200

    mailtrap

--- app/Actions/mailer__config.php
+++ app/Actions/mailer__config.php
@@ -16,25 +16,15 @@
         )
     );
 
-    // $php_mailer->CharSet = "utf-8";
-    // // $php_mailer->SMTPDebug = 2;
-
-    // $php_mailer->Username   = "";
-    // $php_mailer->Password   = "";
-
-    // $php_mailer->Host       = 'smtp.gmail.com';
-    // $php_mailer->Port       = 587;
-    // $php_mailer->isHTML(true);
-
     $php_mailer->CharSet = "utf-8";
     // $php_mailer->SMTPDebug = 2;
 
     // https://mailtrap.io
-    $php_mailer->Username   = "ecc433c2e32e5b";
-    $php_mailer->Password   = "89bfe53cb51d39";
+    $php_mailer->Username   = "";
+    $php_mailer->Password   = "";
 
-    $php_mailer->Host       = 'smtp.mailtrap.io';
-    $php_mailer->Port       = 2525;
+    $php_mailer->Host       = 'smtp.gmail.com';
+    $php_mailer->Port       = 587;
     $php_mailer->isHTML(true);
 
     $email_to = get_bloginfo('admin_email');
