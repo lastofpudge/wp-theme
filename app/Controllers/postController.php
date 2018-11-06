8b1bbb2183612f40e978c407ecda6bd26e0dc951
Andrey <iluxor@yandex.ru>
2018-11-06 22:03:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Controllers/postController.php
+++ app/Controllers/postController.php
@@ -6,9 +6,11 @@ use Timber;
 
 class postController extends Controller
 {
-    public function __construct() {
+    public function __construct()
+    {
         $returned_data = parent::getData();
     }
+
     /*
      * get post data
      */
