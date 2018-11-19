83b4f50b3f6af2cd8e4f7bde8e05233ed7362047
Andrey D <iluxor1991@gmail.com>
2018-11-20 01:44:22 +0200

    improvs a bit

--- core/PostType.php
+++ core/PostType.php
@@ -20,4 +20,32 @@ class PostType
             ]
         );
     }
+
+    public static function loadtax($post_type, $tax_url, $tax_name) {
+        $labels = array(
+          'name' => _x($tax_name, 'taxonomy general name'),
+          'singular_name' => _x('Категорий', 'taxonomy singular name'),
+          'search_items' =>  __('Поиск'),
+          'all_items' => __('Все категории'),
+          'parent_item' => __('Родительская категория'),
+          'parent_item_colon' => __('Родительская категория:'),
+          'edit_item' => __('Edit'),
+          'update_item' => __('Update'),
+          'add_new_item' => __('Add'),
+          'new_item_name' => __('New'),
+          'menu_name' => __('Категории'),
+        );
+
+        register_taxonomy($tax_url, array($post_type), array(
+            'hierarchical' => true,
+            'public' =>  true,
+            'labels' => $labels,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'show_in_nav_menus' => true,
+            'publicly_queryable' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+          ));
+    }
 }
