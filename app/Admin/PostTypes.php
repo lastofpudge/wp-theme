86594740acd282bf90d6a4a5e0a9114312b2c5d1
Andrey D <iluxor1991@gmail.com>
2018-11-20 01:44:41 +0200

    rem def

--- app/Admin/PostTypes.php
+++ app/Admin/PostTypes.php
@@ -8,12 +8,12 @@ add_action('init', function () {
   /*
    * [$url, $name, $singular, $public, $has_archive, $menu_icon, $supports] - paramas.
    */
-  PostType::register('uslugi', 'Услуги', 'Услуга', true, true, 'dashicons-format-aside', ['title', 'editor', 'thumbnail']);
+  // PostType::register('uslugi', 'Услуги', 'Услуга', true, true, 'dashicons-format-aside', ['title', 'editor', 'thumbnail']);
 
   /*
    * [$post_Type, $post_tax, $tax_cat_name] - paramas.
    */
-  PostType::loadtax('uslugi', 'uslugi_cat', 'Категории');
+  // PostType::loadtax('uslugi', 'uslugi_cat', 'Категории');
   //   flush_rewrite_rules();
 
 
