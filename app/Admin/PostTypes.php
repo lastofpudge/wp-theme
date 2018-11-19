aa8c131ac3a477e798bb7cbd8562dc519a8aacd4
Andrey <iluxor@yandex.ru>
2018-11-19 23:44:32 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Admin/PostTypes.php
+++ app/Admin/PostTypes.php
@@ -8,13 +8,11 @@ add_action('init', function () {
   /*
    * [$url, $name, $singular, $public, $has_archive, $menu_icon, $supports] - paramas.
    */
-  PostType::register('uslugi', 'Услуги', 'Услуга', true, true, 'dashicons-format-aside', ['title', 'editor', 'thumbnail']);
+    PostType::register('uslugi', 'Услуги', 'Услуга', true, true, 'dashicons-format-aside', ['title', 'editor', 'thumbnail']);
 
-  /*
-   * [$post_Type, $post_tax, $tax_cat_name] - paramas.
-   */
-  PostType::loadtax('uslugi', 'uslugi_cat', 'Категории');
-  //   flush_rewrite_rules();
-
-
+    /*
+     * [$post_Type, $post_tax, $tax_cat_name] - paramas.
+     */
+    PostType::loadtax('uslugi', 'uslugi_cat', 'Категории');
+//   flush_rewrite_rules();
 }, 0);
--- app/CarbonFields/Models/about.php
+++ app/CarbonFields/Models/about.php
@@ -3,9 +3,9 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make( 'post_meta', __( 'Section Options', 'crb' ) )
-    ->where( 'post_type', '=', 'page' )
-    ->where( 'post_template', '=', 'template-about.php' )
-    ->add_fields(array(
-        Field::make('rich_text', 'new_short_text', 'Описание на главной')
-));
+Container::make('post_meta', __('Section Options', 'crb'))
+    ->where('post_type', '=', 'page')
+    ->where('post_template', '=', 'template-about.php')
+    ->add_fields([
+        Field::make('rich_text', 'new_short_text', 'Описание на главной'),
+]);
