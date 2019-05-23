a1c6e65839b41d60ce1677d0ad390f3c711fb64d
Andrey <iluxor@yandex.ru>
2019-05-23 10:37:47 +0300

    fix wp-load url

--- app/Actions/ajax.php
+++ app/Actions/ajax.php
@@ -2,7 +2,12 @@
 
 header('Content-type: text/html; charset=utf-8');
 
-require_once ABSPATH.'/wp-load.php';
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php')){
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
+}else{
+    $root = explode('/wp-content/', dirname(__FILE__));
+    include_once(str_replace('wp-content/' , '', $root[0] . '/wp-load.php'));
+}
 
 if (isset($_POST['action']) && !empty($_POST['action']) && !is_admin()) {
     $action = $_POST['action'];
