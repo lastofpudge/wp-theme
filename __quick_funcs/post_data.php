8b57ad6f090c19fef015766ec9a90816d25adfda
Andrey <iluxor@yandex.ru>
2019-05-23 07:37:54 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- __quick_funcs/post_data.php
+++ __quick_funcs/post_data.php
@@ -1,20 +1,19 @@
 <?php
         //
-         if (!empty($_POST['test']))
-         {
-            $test = sanitize_text_field($_GET['test']);
-            $this->returned_data['posts'] = Timber::get_posts(
-                array(
-                    'post_type' => 'otzivi',
+         if (!empty($_POST['test'])) {
+             $test = sanitize_text_field($_GET['test']);
+             $this->returned_data['posts'] = Timber::get_posts(
+                [
+                    'post_type'      => 'otzivi',
                     'posts_per_page' => 7,
-                    'paged' => $paged,
-                    'order' => 'DESC',
-                    'meta_query' => array(
-                        array(
-                                'key' => 'test', // for custom fields use: "_fieldsname"
-                                'value' => $test,
-                                'compare' => 'LIKE'
-                            )
-                    ),
-                ));
+                    'paged'          => $paged,
+                    'order'          => 'DESC',
+                    'meta_query'     => [
+                        [
+                                'key'     => 'test', // for custom fields use: "_fieldsname"
+                                'value'   => $test,
+                                'compare' => 'LIKE',
+                            ],
+                    ],
+                ]);
          }
