c4b1215cb06b7ba664aebc5d9c1ee79e7021fd45
unknown <iluxor@yandex.ru>
2020-03-22 18:32:25 +0200

    update packs/remove unused

--- __quick_funcs/work_with_images.php
+++
@@ -1,4 +0,0 @@
-{# Arbitrary resizing of images #}
-<img src="{{ post.thumbnail.src|resize(300, 200) }}" />
-{# Converting images #}
-<img src="{{ post.thumbnail.src|tojpg }}" />
--- core/Autoload.php
+++ core/Autoload.php
@@ -3,14 +3,12 @@
 $data = [
     'helpers'             => __DIR__.'/Helpers.php',
     'config'              => __DIR__.'/../config/app.php',
-    'config_run'          => __DIR__.'/../config/app_run.php',
     'post_register_types' => __DIR__.'/PostType.php',
     'admin_options'       => __DIR__.'/../app/Admin/AdminOptions.php',
-    'mails'               => __DIR__.'/../app/Mail/mail.php',
+    'ajaxs'               => __DIR__.'/../app/Actions/ajax.php',
     'hidden_wp_data'      => __DIR__.'/../app/Admin/HiddenData.php',
     'theme_options'       => __DIR__.'/../app/CarbonFields/OptionFields.php',
     'post_fields'         => __DIR__.'/../app/CarbonFields/PostFields.php',
-    'cat_fields'          => __DIR__.'/../app/CarbonFields/CategoryFields.php',
     'post_types'          => __DIR__.'/../app/Admin/PostTypes.php',
     'share_slugs'         => __DIR__.'/ShareSlugs.php',
 ];
