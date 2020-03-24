0ef43674988002c885f4c88581ac306f8a8aa541
unknown <iluxor@yandex.ru>
2020-03-24 02:41:35 +0200

    remove unused

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Routes::map('api/test', function($params){
-//     dd($_GET);
-//     // Routes::load('archive.php', null, $query, 200);
-// });
--- taxonomy-test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Template Name: Таксономия для услуга**
- */
-
-// require_once(__DIR__ . '/Controllers/categoryController.php');
-
-// Timber::render(array('/categories/category.twig'), $d->index());
