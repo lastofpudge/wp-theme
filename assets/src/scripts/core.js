6cd0c1cdcd8ef2c55a8f244d4f23fda42c46036b
Andrey <iluxor@yandex.ru>
2020-06-23 01:59:53 +0300

    Update core.js

--- assets/src/scripts/core.js
+++ assets/src/scripts/core.js
@@ -1,63 +1,61 @@
-window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require("jquery");
 
-require('magnific-popup');
+require("magnific-popup");
 // require('parsleyjs');
 // require('slick-carousel');
-require('./mmenu.js');
-window.Noty = require('noty');
+// require('./mmenu.js');
+window.Noty = require("noty");
 
 // window.chosen = require('chosen-js');
 // window.AOS = require('aos');
 
-
 $(function() {
-    //popup
-  $('.js-mfi').each(function(index, el) {
+  //popup
+  $(".js-mfi").each(function(index, el) {
     var _el = $(this);
     _el.magnificPopup({
       removalDelay: 500,
       showCloseBtn: false,
       callbacks: {
         beforeOpen: function() {
-          var effect = this.st.el.attr('data-effect');
-          if (effect) this.st.mainClass = this.st.el.attr('data-effect');
-          else this.st.mainClass = 'mfp-with-fade';
-        }
+          var effect = this.st.el.attr("data-effect");
+          if (effect) this.st.mainClass = this.st.el.attr("data-effect");
+          else this.st.mainClass = "mfp-with-fade";
+        },
       },
       items: {
-        src: $(this).data('url'),
-        type: 'inline'
+        src: $(this).data("url"),
+        type: "inline",
       },
-      midClick: true
+      midClick: true,
     });
   });
 
   //image
-  $('.js-mfi-img').magnificPopup({
-    type: 'image',
+  $(".js-mfi-img").magnificPopup({
+    type: "image",
     gallery: {
       enabled: true,
       navigateByImgClick: true,
       preload: [0, 1],
-      tCounter: '%curr% из %total%',
+      tCounter: "%curr% из %total%",
     },
     image: {
       tError: '<a href="%url%">Картинка #%curr%</a> не может быть загружена.',
       verticalFit: true,
       titleSrc: function(item) {
-        return item.el.attr('title');
-      }
+        return item.el.attr("title");
+      },
     },
-    mainClass: 'mfp-no-margins mfp-with-zoom'
+    mainClass: "mfp-no-margins mfp-with-zoom",
   });
 
   //image
-  $('.js-mfi-vid').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
+  $(".js-mfi-vid").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: false,
-    mainClass: 'mfp-no-margins mfp-with-zoom'
+    mainClass: "mfp-no-margins mfp-with-zoom",
   });
-
 });
