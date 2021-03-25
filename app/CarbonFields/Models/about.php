c317da9a563ad1bcd22be7a63f6a9711776f2d51
Andrey <iluxor@yandex.ru>
2021-03-25 03:27:16 +0200

    Update about.php

--- app/CarbonFields/Models/about.php
+++ app/CarbonFields/Models/about.php
@@ -5,7 +5,7 @@ use Carbon_Fields\Field;
 
 // Container::make('post_meta', __('Поля для About', 'crb'))
 //     ->where('post_type', '=', 'page')
-//     ->where('post_template', '=', 'views_php/template-about.php')
+//     ->where('post_template', '=', 'template-about.php')
 //     ->add_fields([
 //         Field::make('rich_text', 'new_short_text', 'Описание на about'),
 //     ]);
