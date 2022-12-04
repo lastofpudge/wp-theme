a5873cb58ffefde9a00674169080f88618128d3e
Andrey Dudin <iluxor@yandex.ru>
2022-12-04 18:34:53 +0200

    refactor: funcs order

--- functions.php
+++ functions.php
@@ -2,20 +2,20 @@
 
 use Carbon_Fields\Carbon_Fields;
 
+const APP_PATH = __DIR__.'/app';
+const BASE_PATH = __DIR__;
+
 if (defined('WP_DEBUG') && true === WP_DEBUG) {
     @ini_set('display_errors', 1);
 }
 
-if (!file_exists(__DIR__.'/core/vendor/autoload.php')) {
+if (!file_exists(BASE_PATH.'/core/vendor/autoload.php')) {
     wp_die('No "autoload.php" file');
 }
 
 add_action('after_setup_theme', function () {
     Carbon_Fields::boot();
 });
-
-const APP_PATH = __DIR__.'/app';
-const BASE_PATH = __DIR__;
 
 require_once BASE_PATH.'/core/vendor/autoload.php';
 
