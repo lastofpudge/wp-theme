1a825a2df7ec687e0505667acd591db9392ed0d6
Andrey Dudin <iluxor@yandex.ru>
2022-12-07 17:55:08 +0200

    refactor: assets fix

--- assets/dist/js/wp_main.js
+++ assets/dist/js/wp_main.js
@@ -1,64 +1,61 @@
-$(function() {
-    const preloader = $('.js-preloader-main');
+$(function () {
+  const preloader = $(".js-preloader-main");
 
-    $(document).on('submit', '#contactForm', function(event) {
-        event.preventDefault();
+  $(document).on("submit", ".js-contact-form", function (event) {
+    event.preventDefault();
 
-        const form = $(this);
-        const form_data = new FormData();
-        // form_data.append( "file", $('#file')[0].files[0]);
+    const form = $(this);
+    const form_data = new FormData();
+    // form_data.append( "file", $('#file')[0].files[0]);
 
-        form_data.append( "name", $(this).find('input[name="name"]').val());
-        form_data.append( "mail", $(this).find('input[name="mail"]').val());
-        form_data.append( "action", "sendMailAction");
-        form_data.append( "nonce", data.nonce);
+    form_data.append("name", $(this).find('input[name="name"]').val());
+    form_data.append("mail", $(this).find('input[name="mail"]').val());
+    form_data.append("action", "sendMailAction");
+    form_data.append("nonce", data.nonce);
 
+    preloader.addClass("js-preloading");
+    $.ajax({
+      type: "POST",
+      url: data.ajax_url,
+      processData: false,
+      contentType: false,
+      data: form_data,
+      success: function (data, text) {
+        preloader.removeClass("js-preloading");
 
-        preloader.addClass('js-preloading');
-        $.ajax({
-            type: "POST",
-            url: data.ajax_url,
-            processData: false,
-            contentType: false,
-            data: form_data,
-            success: function(data, text) {
-                preloader.removeClass('js-preloading');
-
-                // $.magnificPopup.close();
-                if (data.type === 'success') {
-                    new Noty({
-                        theme: 'mint',
-                        text: data.message,
-                        timeout: 5000,
-                        progressBar: false,
-                        closeWith: ['click', 'button'],
-                    }).show();
-                }else{
-                    console.warn(data);
-                    new Noty({
-                        theme: 'mint',
-                        type: 'error',
-                        ttext: data.sended?.errors?.wp_mail_failed[0] || data.message,
-                        timeout: 5000,
-                        progressBar: false,
-                        closeWith: ['click', 'button'],
-                    }).show();         
-                }
-            },
-            fail: function(errors) {
-                preloader.removeClass('js-preloading');
-                new Noty({
-                    theme: 'mint',
-                    text: 'Error send email',
-                    timeout: 5000,
-                    progressBar: false,
-                    closeWith: ['click', 'button'],
-                }).show();
-                console.log(errors);
-            }
-        });
-
-        form.trigger('reset');
+        // $.magnificPopup.close();
+        if (data.type === "success") {
+          new Noty({
+            theme: "mint",
+            text: data.message,
+            timeout: 5000,
+            progressBar: false,
+            closeWith: ["click", "button"],
+          }).show();
+        } else {
+          new Noty({
+            theme: "mint",
+            type: "error",
+            text: data.sended?.errors?.wp_mail_failed[0] || data.message,
+            timeout: 5000,
+            progressBar: false,
+            closeWith: ["click", "button"],
+          }).show();
+        }
+      },
+      fail: function (errors) {
+        preloader.removeClass("js-preloading");
+        new Noty({
+          theme: "mint",
+          text: "Error send email",
+          timeout: 5000,
+          progressBar: false,
+          closeWith: ["click", "button"],
+        }).show();
+        console.log(errors);
+      },
     });
 
+    form.trigger("reset");
+  });
 });
