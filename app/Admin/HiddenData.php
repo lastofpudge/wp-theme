fffb3a84dc9557cc066a50b710248f7d5c9a415f
lastofpudge <iluxor@yandex.ru>
2023-03-04 04:58:53 +0200

    fix gutenberg blocks styles display

--- app/Admin/HiddenData.php
+++ app/Admin/HiddenData.php
@@ -44,6 +44,5 @@ class HiddenData
         wp_dequeue_style('global-styles');
         wp_dequeue_style('wp-block-library');
         wp_dequeue_style('wp-block-library-theme');
-        wp_dequeue_style('storefront-gutenberg-blocks');
     }
 }
--- app/CarbonFields/Blocks/example.php
+++ app/CarbonFields/Blocks/example.php
@@ -3,6 +3,12 @@
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
+wp_register_style(
+	'crb-my-shiny-gutenberg-block-stylesheet',
+	get_stylesheet_directory_uri() . '../app/CarbonFields/Blocks/example.css'
+);
+
+
 Block::make(__('My Gutenberg Block'))
     ->add_fields([
         Field::make('text', 'heading', __('Block Heading')),
@@ -10,6 +16,7 @@ Block::make(__('My Gutenberg Block'))
         Field::make('rich_text', 'content', __('Block Content')),
     ])
     ->set_description(__('Description for the block'))
+    ->set_editor_style( 'crb-my-shiny-gutenberg-block-stylesheet' )
     ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
-        include_once(__DIR__ . '/example.view.php');
+        include(__DIR__ . '/example.view.php');
     });
