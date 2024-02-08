d3d2a247620a1f02c9f32ba17f39e7b3f135614c
lastofpudge <iluxor1991@gmail.com>
2024-02-08 04:09:50 +0200

    refactor: remove unused

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
-let mix = require("laravel-mix");
+const mix = require("laravel-mix");
 
 mix.setPublicPath("resources/assets/dist");
 mix.disableNotifications();
 
 mix
-  .sass("resources/assets/src/styles/bundle.scss", "resources/assets/dist/css/bundle.min.css")
-  .options({
-    processCssUrls: false,
-    cssNano: { minifyFontValues: false },
-  })
-  .js("resources/assets/src/scripts/app.js", "resources/assets/dist/js/app.min.js");
+    .sass("resources/assets/src/styles/bundle.scss", "resources/assets/dist/css/bundle.min.css")
+    .options({
+        processCssUrls: false,
+        cssNano: {minifyFontValues: false},
+    })
+    .js("resources/assets/src/scripts/app.js", "resources/assets/dist/js/app.min.js");
