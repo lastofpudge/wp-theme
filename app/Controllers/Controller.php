86e76da0b053883bbde05466092d7b92ca03f184
lastofpudge <iluxor1991@gmail.com>
2024-02-18 06:06:09 +0200

    refactor: views format

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,16 +6,6 @@ class Controller
 {
     public function __construct()
     {
-        add_filter('timber/context', [$this, 'addBreadcrumbs']);
-    }
 
-    public function addBreadcrumbs($context): array
-    {
-        $context['breads'] = '';
-        if (function_exists('yoast_breadcrumb')) {
-            $context['breads'] = yoast_breadcrumb('<div class="breads-wrapper">', '</div>');
-        }
-
-        return $context;
     }
 }
