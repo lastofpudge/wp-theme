89402f99baff6e3085578051e309631fdc115b97
lastofpudge <iluxor1991@gmail.com>
2025-09-15 01:38:27 +0300

    feat: build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import autoprefixer from 'autoprefixer'
+import { copyFileSync, existsSync, mkdirSync } from 'fs'
+import pkg from 'glob'
+const { glob } = pkg
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  publicDir: false,
+  plugins: [
+    {
+      name: 'copy-images',
+      writeBundle() {
+        const srcDir = 'resources/assets/src/images'
+        const destDir = 'resources/assets/dist/images'
+
+        if (!existsSync(destDir)) {
+          mkdirSync(destDir, { recursive: true })
+        }
+
+        const files = glob.sync(`${srcDir}/**/*`, { nodir: true })
+        files.forEach(file => {
+          const relativePath = file.replace(srcDir + '/', '')
+          const destFile = `${destDir}/${relativePath}`
+          const destFileDir = dirname(destFile)
+
+          if (!existsSync(destFileDir)) {
+            mkdirSync(destFileDir, { recursive: true })
+          }
+
+          copyFileSync(file, destFile)
+        })
+      }
+    }
+  ],
   build: {
     outDir: 'resources/assets/dist',
     emptyOutDir: true,
@@ -14,8 +47,11 @@ export default defineConfig({
       output: {
         entryFileNames: 'js/[name].min.js',
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
+          if (assetInfo.names?.[0]?.endsWith('.css')) {
             return 'css/bundle.min.css'
+          }
+          if (assetInfo.names?.[0]?.match(/\.(png|jpe?g|svg|gif|webp|ico)$/)) {
+            return 'images/[name][extname]'
           }
           return 'assets/[name][extname]'
         }
